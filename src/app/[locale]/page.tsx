11e672082025fc28f2a73cc4994279e5325c9d52
MarkusLindner123 <markuslindner1998@gmail.com>
2025-08-17 11:32:14 +0000

    sticky slides removed

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,12 @@
 import { setRequestLocale } from "next-intl/server";
 import { routing } from "@/i18n/routing";
 
-import LanguageSwitcher from "@/components/LanguageSwitcher";
-import StickySlides from "@/components/StickySlides";
+//import LanguageSwitcher from "@/components/LanguageSwitcher";
+//import StickySlides from "@/components/StickySlides";
+import HeroVideo from "@/components/HeroVideo";
+import Hero from "@/components/Hero";
+import AboutSection from "@/components/AboutSection";
+import ServicesSection from "@/components/ServicesSection";
 
 export default async function HomePage({
   params,
@@ -13,13 +17,29 @@ export default async function HomePage({
   setRequestLocale(locale);
 
   return (
-    <main className="min-h-screen bg-brand-bg text-brand-text font-sans">
-      <StickySlides />
-
-      {/* Anything after the stacked slides will come *after* the takeover sequence 
-      <section className="max-w-7xl mx-auto px-6 py-20 flex justify-center">
-        <LanguageSwitcher currentLocale={locale} />
-      </section>*/}
-    </main>
+<div className="relative p-6 space-y-6 md:p-12 md:space-y-12">
+    <section className=" top-6 min-h-screen z-20 bg-brand-bg rounded-2xl overflow-hidden shadow-xl">
+       
+          <HeroVideo />
+      </section>
+        <section className=" top-6 min-h-screen z-20 bg-brand-bg rounded-2xl overflow-hidden shadow-xl">
+       
+          <Hero/>
+      
+      </section>
+       {/* Slide 4: Services */}
+      <section className=" top-6 min-h-screen z-20 bg-brand-bg rounded-2xl overflow-hidden shadow-xl">
+       
+          <AboutSection />
+      
+      </section>
+      
+      {/* Slide 4: Services */}
+      <section className=" top-6 min-h-screen z-20 bg-brand-bg rounded-2xl overflow-hidden shadow-xl">
+       
+          <ServicesSection />
+      
+      </section>
+    </div>
   );
 }
