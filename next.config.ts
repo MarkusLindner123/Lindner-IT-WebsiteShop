5cc7684a5eecdac055f18504079c33193be6aca2
MarkusLindner123 <markuslindner1998@gmail.com>
2025-08-19 09:16:14 +0200

    test mit next.config ob wieder redirect funktioniert

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  basePath: "/Lindner-IT-WebsiteShop",    // GitHub Pages subpath
-  assetPrefix: "/Lindner-IT-WebsiteShop/", 
-  images: { unoptimized: true },
+  //reactStrictMode: true,
+  //output: "export",
+  //basePath: "/Lindner-IT-WebsiteShop",    // GitHub Pages subpath
+  //assetPrefix: "/Lindner-IT-WebsiteShop/", 
+  //images: { unoptimized: true },
 };
 
 const withNextIntl = createNextIntlPlugin();
