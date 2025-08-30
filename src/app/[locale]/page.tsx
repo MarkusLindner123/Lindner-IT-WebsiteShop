6fc2ec4d30f0519a6745a5ed292b5a40ac6f4938
MarkusLindner123 <markuslindner1998@gmail.com>
2025-08-30 11:02:39 +0000

    testimonials fertig

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,6 +8,7 @@ import Hero from "@/components/Hero";
 import AboutSection from "@/components/AboutSection/AboutSection";
 import ServicesSection from "@/components/ServicesSection";
 import ContactSection from "@/components/ContactSection";
+import TestimonialsSection from "@/components/TestimonialsSection"; // <-- neue Komponente
 
 export default async function HomePage({
   params,
@@ -18,9 +19,7 @@ export default async function HomePage({
   setRequestLocale(locale);
 
   return (
-    // Use a <main> tag for the main content
     <main className="relative p-6 space-y-6 md:p-12 md:space-y-12">
-      {/* ID for the "Home" link */}
       <div className="space-y-6 md:space-y-12">
         <section className="bg-heroVideo-bg rounded-2xl overflow-hidden shadow-xl">
           <HeroVideo />
@@ -29,21 +28,28 @@ export default async function HomePage({
           <Hero />
         </section>
       </div>
-      
-      {/* ID for the "About" link */}
-      <section id="about" className="bg-about-bg rounded-2xl overflow-hidden shadow-xl">
-        <AboutSection />
-      </section>
-      
-      {/* ID for the "Services" link */}
+
+  
+
       <section id="services" className="bg-services-section-bg rounded-2xl overflow-hidden shadow-xl">
         <ServicesSection />
       </section>
-      
-      {/* ID for the "Contact" link */}
-      <section id="contact" className="bg-contact-bg rounded-2xl overflow-hidden shadow-xl">
+
+      <section id="about" className="bg-about-bg rounded-2xl overflow-hidden shadow-xl">
+        <AboutSection />
+      </section>
+
+ 
+
+      {/* Testimonials Section ganz unten */}
+      <section id="testimonials" className="bg-location-bg rounded-2xl overflow-hidden shadow-xl">
+        <TestimonialsSection />
+      </section>
+
+     <section id="contact" className="bg-contact-bg rounded-2xl overflow-hidden shadow-xl">
         <ContactSection />
       </section>
+
     </main>
   );
 }
